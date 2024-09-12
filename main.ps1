dcf77379df2f328b8eb25b686b67d4fc3d236b99
xjessxx <144179624+xjessxx@users.noreply.github.com>
2024-09-12 17:49:36 -0400

    Add files via upload

---
+++ main.ps1
@@ -0,0 +1,11 @@
+﻿. (Join-Path $PSScriptRoot functionseventslogs.ps1)
+
+clear
+
+#Get login and logoffs from the last 14 days
+$loginoutsTable = customTable -numDays $numDays
+$loginoutsTable
+
+#get startup and shutdowns from last 14 days
+$shutdownstartupTable = startAndShut -numDays $numDays
+$shutdownstartupTable
