6a4e323d3b59f8119cafb5f060bf5635569763c3
xjessxx <144179624+xjessxx@users.noreply.github.com>
2024-11-15 19:46:58 -0500

    Add files via upload

---
+++ assignment1.bash
@@ -0,0 +1,102 @@
+#! /bin/bash
+clear
+
+# filling courses.txt
+bash courses.bash
+
+courseFile="courses.txt"
+
+function displayCoursesofInst(){
+
+echo -n "Please Input an Instructor Full Name: "
+read instName
+
+echo ""
+echo "Courses of $instName :"
+cat "$courseFile" | grep "$instName" | cut -d';' -f1,2 | \
+sed 's/;/ | /g'
+echo ""
+
+}
+
+function courseCountofInsts(){
+
+echo ""
+echo "Course-Instructor Distribution"
+cat "$courseFile" | cut -d';' -f7 | \
+grep -v "/" | grep -v "\.\.\." | \
+sort -n | uniq -c | sort -n -r 
+echo ""
+
+}
+
+# TODO - 1
+# Make a function that displays all the courses in given location
+# function dislaplays course code, course name, course days, time, instructor
+# Add function to the menu
+# Example input: JOYC 310
+# Example output: See the screenshots in canvas
+function displayCourseLocal(){
+echo""
+echo -n "please input a location ex: JOYC 310: "
+read location
+
+echo "information of instruction for $location"
+cat "$courseFile" | grep "$location" | cut -d';' -f1,2,5,6,7 | \
+sed 's/;/ | /g'
+echo""
+}
+
+
+# TODO - 2
+# Make a function that displays all the courses that has availability
+# (seat number will be more than 0) for the given course code
+# Add function to the menu
+# Example input: SEC
+# Example output: See the screenshots in canvas
+function displayAvailability(){
+echo""
+echo -n "please input a subject name ex: SEC : "
+read code
+
+echo "available courses in  $code"
+cat "$courseFile" | grep "$code" | awk '$4 > 0' | cut -d';' -f1,2,3,5,6,7,8,9,10 | \
+sed 's/;/ | /g'
+echo""
+}
+
+while :
+do
+	echo ""
+	echo "Please select and option:"
+	echo "[1] Display courses of an instructor"
+	echo "[2] Display course count of instructors"
+	echo "[3] Display courses of a location"
+	echo "[4] Display available courses of subject"
+	echo "[5] Exit"
+
+	read userInput
+	echo ""
+
+	if [[ "$userInput" == "5" ]]; then
+		echo "Goodbye"
+		break
+
+	elif [[ "$userInput" == "1" ]]; then
+		displayCoursesofInst
+
+	elif [[ "$userInput" == "2" ]]; then
+		courseCountofInsts
+
+	elif [[ "$userInput" == "3" ]]; then
+                 displayCourseLocal
+
+	elif [[ "$userInput" == "4" ]]; then
+                 displayAvailability
+
+	# TODO - 3 Display a message, if an invalid input is given
+	else
+		echo "enter a number 1-5"
+	fi
+done
+ 
